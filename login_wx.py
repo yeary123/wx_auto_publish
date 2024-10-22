729e7052d99aba2a623c8be5aec7523021871b73
qianli <qianli@hillinsight.com>
2024-10-22 15:57:37 +0800

    发布流程优化

--- login_wx.py
+++ login_wx.py
@@ -77,7 +77,8 @@ class login_wx(wx):
             'title':'1211212',
             'author':'222',
             'content':'fffegewqgeq',
-            'img':''
+            'img':'/Users/liqian/Documents/wireshark.jpeg',
+            'time':'',
         }
         
     async def login(self) -> None:
@@ -109,7 +110,7 @@ class login_wx(wx):
             await new_page.keyboard.press('Tab')  
             await new_page.keyboard.type(self.dialog['author'])
             time.sleep(1)
-            # 填写正文
+            # 填写正文（光标focus在正文部分）
             await new_page.keyboard.press('Tab') 
             #   在开头插入图片 上传
             upload_pic = await new_page.wait_for_selector('#js_editor_insertimage')
@@ -118,8 +119,9 @@ class login_wx(wx):
             # 等待并点击触发文件选择器的按钮  
             new_page.on("filechooser", lambda file_chooser: file_chooser.set_files(self.dialog['img']))
             # 点击触发文件选择器的按钮  
-            await new_page.click('#js_editor_insertimage > ul > li:nth-child(1)')             
-            time.sleep(1)
+            upload_icon = await new_page.wait_for_selector('#js_editor_insertimage > ul > li:nth-child(1)')   
+            await upload_icon.click()          
+            await asyncio.sleep(1)
             #   在开头插入图片 不上传
             # pick_pic = await new_page.wait_for_selector('//*[@id="js_editor_insertimage"]')
             # await pick_pic.click()
@@ -134,11 +136,15 @@ class login_wx(wx):
             # await sure.click()
             # time.sleep(1)
             #    写入正文  
+            await new_page.keyboard.type('')
+            time.sleep(5)
+            await new_page.keyboard.type('')
+            time.sleep(5)
             await new_page.keyboard.type(self.dialog['content'])
             time.sleep(1)
             # 滚动到页面底部  
-            new_page.evaluate('window.scrollTo(0, document.body.scrollHeight);')  
-  
+            await new_page.evaluate('window.scrollTo(0, document.body.scrollHeight);')  
+            time.sleep(1)
             # 选择第一张图片做封面
             pic = await new_page.wait_for_selector('//*[@id="js_cover_area"]/div[1]')
             await pic.hover()
@@ -155,11 +161,23 @@ class login_wx(wx):
             finish_btn = await new_page.wait_for_selector('//*[@id="vue_app"]/div[2]/div[1]/div/div[3]/div[2]/button')
             await finish_btn.click()
             time.sleep(5)
-            # 进入发布流程
+            # 进入定时发布流程
             publish_btn = await new_page.wait_for_selector('//*[@id="js_send"]/button')
             await publish_btn.click()
             time.sleep(1)
-
+            set_publish_time = await new_page.wait_for_selector('#vue_app > div:nth-child(5) > div.new_mass_send_dialog > div.weui-desktop-dialog__wrp > div > div.weui-desktop-dialog__bd > div > div > form > div.mass-send__td-setting.timer_setting > div > div > div.mass-send__timer-wrp')
+            await set_publish_time.click()
+            # 找到输入框并输入发布时间
+            input_time_icon = await new_page.wait_for_selector('#vue_app > div:nth-child(5) > div.new_mass_send_dialog > div.weui-desktop-dialog__wrp > div > div.weui-desktop-dialog__bd > div > div > form > div.mass-send__td-setting.timer_setting > div.mass-send__timer-container > div > dl:nth-child(2)')  
+            await input_time_icon.click()
+            time.sleep(1)
+            await new_page.fill('#vue_app > div:nth-child(5) > div.new_mass_send_dialog > div.weui-desktop-dialog__wrp > div > div.weui-desktop-dialog__bd > div > div > form > div.mass-send__td-setting.timer_setting > div.mass-send__timer-container > div > dl.weui-desktop-picker__time.weui-desktop-picker__focus > dt > span > div > span > input', '') 
+            await new_page.keyboard.type('15:30')
+            time.sleep(1)
+            # await new_page.click('body:not(:has(select#mySelect.open))')  # 这是一个伪代码示例，实际选择器可能需要根据页面结构进行调整
+            input_time_icon1 = await new_page.wait_for_selector('#vue_app > div:nth-child(5) > div.new_mass_send_dialog > div.weui-desktop-dialog__wrp > div > div.weui-desktop-dialog__bd > div > div > form > div.mass-send__td-setting.timer_setting > div.mass-send__timer-container > div > dl.weui-desktop-picker__time.weui-desktop-picker__focus > dt > i')
+            await input_time_icon1.click()
+            time.sleep(1)
             publish_again = await new_page.wait_for_selector('//*[@id="vue_app"]/div[2]/div[1]/div[1]/div/div[3]/div/div/div[1]/button')
             await publish_again.click()
             time.sleep(1)
