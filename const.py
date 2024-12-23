9af09fe6fbd4375a92e8b9f076c88fa9afeff301
Qian <qian@mini.local>
2024-12-23 16:05:43 +0800

    优化

--- const.py
+++ const.py
@@ -1,15 +1,4 @@
-API_KEY = ['sk-IoKHaUlkTa12VbMkIZSyAGdlLVogZ7HCELkuoLkMVwXUcyjd',
-       'sk-YYov32ZjPCD1bN9AG44zwmQJxMJABSAeHCrjZHoT6K5A7LmV',
-       'sk-faToKywH9lLKlmEIiPT3n7zw2dIfnshx9h74SOkFaItVQ604',
-       'sk-61TKoPC9JDizDvfptKSNvvj5VerE8TX2WSL4EJMyYtPriAXV',
-       'sk-X19cW4JCtmPli347wp77rHWd4Fo37SCeQQ6UGlVi0Cf9jMgt',
-       'sk-STJaxamXI57i7bg3bnR2UxWeua38n7Ju2hAewtlCIBrc7YyL',
-       'sk-zAI1hi1OfBNNtub7pYFJXP14CcplaNbxxpBFb5rCCtlybH08',
-       'sk-MGZx8rEdbIKYLNQ3hS95ofH7aIum2Nu7SyNQyPvmouH7Xg9G',
-       'sk-2dBpmBlhUctyWFhynfnlvYu1t5UC6lA4LS022CNPK7Kmpjwa',
-       'sk-MoznfJX5PsrGDCDZpgi4BOGoapHvCfQW43zAMhVbhFiJBRLm',
-       'sk-ukAAC85Z01TkfQpV6VRAEuuIrbJJzHti4mwIH8oAnOb5OofI',
-       'sk-2H49yH7dlVwMYNBMtCjFtfsTvugdBHvCh8HK0ZyUeFTBy5kg'
+API_KEY = [
        ]
 
 UA = {
@@ -30,10 +19,10 @@ HOT = "hot"
 ENTERTAINMENT = "entertainment"
 
 author_and_type = {
-    '小北': HOT,
-    '小蛙': HOT,
-    '在花': SPORTS,
-    '李十一': HISTORY,
+    'aa': HOT,
+    'bb': HOT,
+    'cc': SPORTS,
+    'ee': HISTORY,
 }
 
 # 每次发布的数量
